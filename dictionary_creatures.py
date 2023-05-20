6b98c0930c30c3477a91026bdd87e81d0d36d766
Federico Cando <133919977+FedericoCWW@users.noreply.github.com>
2023-05-20 18:08:33 -0300

    Add files via upload

---
+++ dictionary_creatures.py
@@ -0,0 +1,55 @@
+import random as rand
+import sys
+#values by order: Name, Hipoints, Size, Alligment
+
+elements = ["Fire", "Water", "Ice", "Earth", "Wind", "Light", "Dark", "Electricity", None]
+alligment = ["Lawful good", "Neutral good", "Chaotic good", "Lawful neutral", "(True) neutral", "Chaotic neutral","Lawful evil", "Neutral evil", "Chaotic evil"]
+sizeAndhitpoints = {
+    "tiny": rand.randint(1, 20),
+    "small": rand.randint(20, 60), 
+    "medium": rand.randint(40, 100),
+    "large": rand.randint(100, 200),
+    "huge": rand.randint(200, 500),
+    "gargantuan": rand.randint(500, 1000)
+}
+
+name = ["Rat", 
+        "Cyclop", 
+        "Evil Gnome", 
+        "Spider", 
+        "Iron Golem", 
+        "Orc Warrior", 
+        "Hell Hound", 
+        "Ogre",
+        "Gargoyle", 
+        "Crab",
+        "Killer Whale",
+        "Narwhal",
+        "Sperm Whale",
+        "Wight",
+        "Dire Wolf",
+        "Wolf",
+        "Wraith",
+        "Wyvern",
+        "Yellow Mold",
+        "Zombie",
+        "Vampire"
+        "Minotaur"
+        "Bandit",
+        "Bandit Leader",
+        "Ant",
+        "Snake",
+        "Pit Fighter",
+        "Ghoul",
+        "Knight",
+        "Acolyte",
+        "Mage",
+        "Archer",
+        "Hell Spawn"
+        "Wendigo",
+        "Scorpion",
+        "WereWolf",
+        "Samurai",
+        "Cursed Chest",
+        "Iron Maiden"
+        ]
---
+++ main.py
@@ -0,0 +1,28 @@
+import random
+import dictionary_creatures
+
+def Generator():
+    n = dictionary_creatures.name
+    a = dictionary_creatures.alligment
+    l = dictionary_creatures.sizeAndhitpoints
+    e = dictionary_creatures.elements
+    key, value = random.choice(list(l.items()))
+    print(f"Name: {random.choice(n)}")
+    print(f"Size: {key}")
+    print(f"Hitpoints: {value}")
+    print(f"Element: {random.choice(e)}")
+    print(f"Alligment: {random.choice(a)}")
+
+def Opt():
+    return str(input("Would You like to Generate RPG Sttufs?(y/n): "))
+
+opt = Opt()
+while(True):
+    if (opt == 'y'):
+        count = int(input("How Many creatures would you like to generate: "))
+        for i in range(count):
+            Generator()
+            print("//////////////////////////////////////////")
+        opt = Opt()
+    else:
+        exit()
